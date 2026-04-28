89760b210c6b3cca3e66a3f869302d5df596b81a
Senait Mekonen Gebreegziabher <konesham1993@gmail.com>
2026-04-28 15:33:41 +0300

    new changes in folder structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Climate Challenge Week 0
+
+## Setup
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+print("Environment is working")
+
+# simple test data
+data = [1, 2, 3, 4, 5]
+df = pd.DataFrame(data, columns=["numbers"])
+
+print(df)
